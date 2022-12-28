73d02357489ec1c23a7a9d4c2359082ab70d4ecc
Azeez Monsur <monsur.koa@gmail.com>
2022-12-28 08:43:36 +0100

    first commmit

---
+++ src/assets/theme/colors.js
@@ -0,0 +1,14 @@
+export const colorThemeMarina = {
+  greenLight: "rgb(197, 218, 170)",
+  greenHeavy: "rgb(123, 179, 50)",
+  greenDark: "rgb(24, 124, 41)",
+  redLight: "rgb(233, 118, 114)",
+  redHeavy: "rgb(233, 40, 34)",
+  inaction: "rgb(140, 143, 140)",
+  defaultWhite: "rgb(255, 255, 255)",
+  greyLight: "rgba(163, 157, 157, 0.9)",
+  greyDark: "rgba(83, 81, 81, 0.9)",
+  eveningDark: "rgba(0, 0, 0, 0.3)",
+  nightDark: "rgba(0, 0, 0, 0.9)",
+  transparent: "inherit",
+};
